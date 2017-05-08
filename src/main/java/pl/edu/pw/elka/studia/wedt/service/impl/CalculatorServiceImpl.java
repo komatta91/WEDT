72f7b68321ba99d8bdf14ea86519e56a041159ad
komatta91 <komatta91@gmail.com>
2017-05-08 20:33:34 +0200

    commit

--- src/main/java/pl/edu/pw/elka/studia/wedt/service/impl/CalculatorServiceImpl.java
+++ src/main/java/pl/edu/pw/elka/studia/wedt/service/impl/CalculatorServiceImpl.java
@@ -1,5 +1,9 @@
 package pl.edu.pw.elka.studia.wedt.service.impl;
 
+import org.la4j.*;
+import org.la4j.Vector;
+import org.la4j.vector.VectorFactory;
+import org.la4j.vector.dense.BasicVector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
@@ -8,8 +12,7 @@ import pl.edu.pw.elka.studia.wedt.service.WikiService;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Komatta on 2017-05-03.
@@ -34,11 +37,53 @@ public class CalculatorServiceImpl implements CalculatorService {
         double numerator = Math.log(Math.max(firstList.size(), secondList.size())) - Math.log(commonList.size());
         double denominator = Math.log(wikiArticlesAmount.doubleValue()) - Math.log(Math.min(firstList.size(), secondList.size()));
 
-        return new BigDecimal(numerator).divide(new BigDecimal(denominator), 5, BigDecimal.ROUND_HALF_EVEN);
+        return new BigDecimal(numerator).divide(new BigDecimal(denominator));
     }
+
+    private BigDecimal angleMeasure(String language, String firstEntry, String secondEntry) {
+        BigInteger wikiArticlesAmount = wikiService.getTotalArticlesNumber(language);
+        List<String> firstList = wikiService.getReferencesOfArticle(language, firstEntry);
+        List<String> secondList = wikiService.getReferencesOfArticle(language, secondEntry);
+        Set<String> distinctLinks = new LinkedHashSet<>(firstList);
+        distinctLinks.addAll(secondList);
+        Map<String, BigDecimal> firstVector = calculateWeights(language, wikiArticlesAmount, distinctLinks, firstList);
+        Map<String, BigDecimal> secondVector = calculateWeights(language, wikiArticlesAmount,distinctLinks, secondList);
+        return calculateAngle(firstVector, secondVector);
+    }
+
+    private Map<String, BigDecimal> calculateWeights(String language,  BigInteger wikiArticlesAmount, Set<String> distinctLinks, List<String> existingList) {
+        Map<String, BigDecimal> result = new HashMap<>(distinctLinks.size());
+
+        for (String link: distinctLinks) {
+            if (!existingList.contains(link)) {
+                result.put(link, new BigDecimal(0));
+            } else {
+                int backLinkCount = wikiService.getReferencesToArticle(language, link).size();
+                result.put(link, new BigDecimal(Math.log(new BigDecimal(wikiArticlesAmount.toString()).divide(new BigDecimal(backLinkCount)).doubleValue())));
+            }
+        }
+        return result;
+    }
+
+    private BigDecimal calculateAngle( Map<String, BigDecimal> firstVector, Map<String, BigDecimal> secondVector) {
+        BasicVector v1 = new BasicVector(firstVector.size());
+        BasicVector v2 = new BasicVector(firstVector.size());
+        int i = 0;
+        for (String vec: firstVector.keySet()) {
+            v1.set(i, firstVector.get(vec).doubleValue());
+            v2.set(i, secondVector.get(vec).doubleValue());
+            i++;
+        }
+        BigDecimal innerProduct = new BigDecimal(v1.innerProduct( v2 ));
+        BigDecimal v1Norm = new BigDecimal(v1.norm());
+        BigDecimal v2Norm = new BigDecimal(v2.norm());
+        BigDecimal radAngle = innerProduct.divide(v1Norm.multiply(v2Norm));
+        return new BigDecimal(Math.acos(radAngle.divide(new BigDecimal(Math.PI)).doubleValue()));
+    }
+
 
     @Override
     public BigDecimal calculate(String language, String firstEntry, String secondEntry) {
-        return googleNormalizedDistance(language, firstEntry, secondEntry);
+        return googleNormalizedDistance(language, firstEntry, secondEntry).add(angleMeasure(language,firstEntry,secondEntry)).divide(new BigDecimal(2), 5, BigDecimal.ROUND_HALF_EVEN);
     }
 }
