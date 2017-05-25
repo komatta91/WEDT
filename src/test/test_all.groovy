64a1127939df35bf8aba88c1ad01f5719bde009e
m168 <6865656+maciej168@users.noreply.github.com>
2017-05-25 23:50:38 +0200

    test

---
+++ src/test/test_all.groovy
@@ -0,0 +1,27 @@
+import com.opencsv.CSVReader
+import groovy.io.FileType
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner
+import org.springframework.context.support.GenericApplicationContext
+import pl.edu.pw.elka.studia.wedt.service.CalculatorService
+
+def ctx = new GenericApplicationContext()
+new ClassPathBeanDefinitionScanner(ctx).scan('pl')
+new XmlBeanDefinitionReader(ctx).loadBeanDefinitions('webapp/WEB-INF/appconfig-mvc.xml','webapp/WEB-INF/appconfig-root.xml')
+ctx.refresh()
+def calculatorService = ctx.getBean(CalculatorService.class)
+
+new File('results.txt').withWriter { out ->
+    new File("resources/datasets/wordpairs").eachFileRecurse(FileType.FILES) { file ->
+        CSVReader reader = new CSVReader(new FileReader(file), ':' as char)
+        String[] nextLine
+        while ((nextLine = reader.readNext()) != null) {
+            println('Starting calculation for ' + nextLine[0..2])
+            def res = calculatorService.calculate("en", nextLine[0], nextLine[1])
+            def resLine = nextLine[0..2] + res.googleDistance + res.angle + res.finalScore
+            println(resLine)
+            out.println(resLine.join(':'))
+        }
+    }
+}
+
